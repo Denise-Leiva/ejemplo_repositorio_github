1f0371c4ef8afb1dc9c9f63dc6234ba52e4d581e
Denise Leiva <denisedleiva@gmail.com>
2023-10-23 21:48:08 -0300

    Agregada clase caballo y oveja

--- terrestes/ganado.py
+++ terrestes/ganado.py
@@ -1,8 +1,13 @@
-from animales import Animal
+from animal import Animal
 
 class Vaca(Animal):
     pass
 
 
 class Bufalo(Animal):
+    pass
+
+class oveja(Animal):
+    pass
+class caballo(Animal):
     pass
